8f3d126a17e2cf067a8db9adbf693c71e68a4f79
Vineo2 <70734570+Vineo2@users.noreply.github.com>
2025-02-27 09:05:26 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- ebacprograma.c
+++ ebacprograma.c
@@ -139,6 +139,7 @@ int main()
 	printf("\t1 - Registrar Nomes\n");
 	printf("\t2 - Consultar Nomes\n");
 	printf("\t3 - Deletar Nomes\n\n"); 
+	printf("\t4 - Sair do Sistema\n\n");
 	printf("Opçao: "); //fim do menu
 	
 	if (scanf("%d", &opcao) != 1) //Verificaçao para ver se a opção selecionada é um número, bugs ocorriam quando era uma letra
@@ -167,6 +168,12 @@ int main()
 	        case 3:
 			 deletar();
 	         break;
+	         
+	         case 4:
+              printf("Obrigado por utilizar o sistema!\n");
+              return 0;
+              break;
+              
 	         
 	        default:
 	         printf("Essa opção não está disponivel!\n");
